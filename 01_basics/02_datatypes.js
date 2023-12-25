79eb503ad0fbcec3003abdba87c1b172eba55ec9
paramnoor321 <69110186+paramnoor321@users.noreply.github.com>
2023-12-25 16:56:28 +0000

    container changes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello") // we are  using nodejs, not browser
+
+let name = "hitesh" //string datatype
+let age = 18 //number datatype
+let isLoggedIn = false //boolean datatype
+let state;
+
+// Primitive data types
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned to variable
+// symbol => unique
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
