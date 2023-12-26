700ab264295b2759cb73636978940d366602eee3
paramnoor321 <69110186+paramnoor321@users.noreply.github.com>
2023-12-26 09:33:36 +0000

    d

--- 01_basics/07_datesinjs.js
+++ 01_basics/07_datesinjs.js
@@ -7,5 +7,20 @@ let myDate = new Date()
 // console.log(myDate.toLocaleDateString());
 // console.log(myDate.toLocaleString());
 
-let myCreatedDate = new Date(2023, 0, 23, 5, 4, 23)
+let myCreatedDate = new Date("01-14-2023")
 console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate.getMonth() + 1);
+
+// `${newDate.getDay()} and the time`
+
+newDate.toLocaleString('default',{
+    weekday: "long",
+    
+})
