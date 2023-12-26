c2808fcc9910f7bbd4b376ef61a089042d98960d
paramnoor321 <69110186+paramnoor321@users.noreply.github.com>
2023-12-26 09:08:16 +0000

    d

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,11 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+let myCreatedDate = new Date(2023, 0, 23, 5, 4, 23)
+console.log(myCreatedDate.toLocaleString());
